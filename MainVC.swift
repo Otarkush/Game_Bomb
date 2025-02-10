0613beba1b62fcf683cfa1998fbd87bcc9f044af
Phan56 <13bp@mail.ru>
2025-02-10 23:35:17 +0300

    test_commit

---
+++ MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  Game_Bomb
+//
+//  Created by Phan56 on 10.02.2025.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
