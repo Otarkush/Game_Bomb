0677719ed95badd8a0ad56b9a50162d2bb300b67
Phan56 <13bp@mail.ru>
2025-02-12 23:40:34 +0300

    mainVC v1 готов

---
+++ Game_Bomb/HelpVC.swift
@@ -0,0 +1,29 @@
+//
+//  HelpVC.swift
+//  Game_Bomb
+//
+//  Created by Phan56 on 11.02.2025.
+//
+
+import UIKit
+
+class HelpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
